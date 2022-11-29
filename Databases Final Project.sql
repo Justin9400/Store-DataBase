37bde5f0425c8cad539ff0ce583ce07306447a42
Justin9400 <57471411+Justin9400@users.noreply.github.com>
2022-11-28 22:39:48 -0500

    Add files via upload

--- Databases Final Project.sql
+++ Databases Final Project.sql
@@ -2,67 +2,113 @@
 CREATE DATABASE OnlinePurchaseDB;
 USE OnlinePurchaseDB;
 
+CREATE TABLE credentials(
+	userId INT,
+	password VARCHAR(255),
+  	PRIMARY KEY(userId)
+);
+
 CREATE TABLE customer(
+	userId INT,
 	customerId INT,
-	username VARCHAR(255),
-	passwrd VARCHAR(255),
     fname VARCHAR(255),
     lname VARCHAR(255),
-    DOB DATE, 
-	Address VARCHAR(255),
+    DOB DATE,
+	Home Addr VARCHAR(255), 
   	PRIMARY KEY(customerID)
 );
-INSERT INTO customer VALUES (12345, "user1", "user1", "John", "Doe", "1993-04-21", "497 Lakewood St");
-INSERT INTO customer VALUES (54321, "user2", "user2", "Jack", "Lloyd", "2000-05-15", "381 College St");
-INSERT INTO customer VALUES (13542, "user3", "user3", "Matt", "Dunn", "1985-01-05", "9101 Main St");
-
-CREATE TABLE items(
-    itemID VARCHAR(255),
-    itemName VARCHAR(255),
-    price FLOAT(7, 2), 
-    numberInStock INT,
-    PRIMARY KEY(itemID)
-);
-INSERT INTO items VALUES (4567, "Pen", 0.75, 100);
-INSERT INTO items VALUES (2345, "Pencil", 0.50, 100);
-INSERT INTO items VALUES (8765, "Notebook", 5.00, 100);
-
-CREATE TABLE discount(
-	itemID INT, 
-	percentOff INT,
-	PRIMARY KEY(itemID)
-);
-INSERT INTO discount VALUES (4567, 20);
-INSERT INTO discount VALUES (2345, 5);
-INSERT INTO discount VALUES (8765, 10);
 
 CREATE TABLE Cart(
 	customerID INT,
 	itemID VARCHAR(255), 
-    price FLOAT(7, 2),
+    date DATE,
     quantity INT,
   	PRIMARY KEY(customerID)
 );
-INSERT INTO Cart VALUES (12345, 4567, 0.75, 1);
-INSERT INTO Cart VALUES (54321, 2345, 0.50, 1);
-INSERT INTO Cart VALUES (13542, 8765, 5.00, 1);
 
-CREATE TABLE bill(
-	acctNo INT, 
-	orderNo INT, 
-	itemId INT, 
-	total FLOAT(20, 2),
-  	PRIMARY KEY(acctNo)
+CREATE TABLE address(
+	Home Addr VARCHAR(255),
+	zipcode INT,
+	city VARCHAR(255),
+  	PRIMARY KEY(Home Addr)
 );
-INSERT INTO bill VALUES (84567, 09345, 4567, 1.25);
-INSERT INTO bill VALUES (23689, 46831, 2345, 5.00);
-INSERT INTO bill VALUES (09364, 07602, 8765, 20.00);
+
+CREATE TABLE bank account( 
+	accNo INT, 
+	customerId INT, 
+	bankName VARCHAR(255),
+	PRIMARY KEY(customerId)
+);
 
 CREATE TABLE suppliers(
-	orderNo INT, 
+	itemId INT, 
 	location VARCHAR(255),
   	PRIMARY KEY(orderNo)
 );
-INSERT INTO suppliers VALUES (09345, 1.25);
-INSERT INTO suppliers VALUES (46831, 5.00);
-INSERT INTO suppliers VALUES (07602, 20.00);
+
+CREATE TABLE items(
+    itemID VARCHAR(255),
+    itemName VARCHAR(255),
+	numberInStock INT,
+    price FLOAT(7, 2), 
+    PRIMARY KEY(itemID)
+);
+
+CREATE TABLE bill(
+	customerID INT,
+	acctNo INT, 
+	orderNo INT, 
+	itemId INT, 
+	zipcode INT,
+	total FLOAT(20, 2),
+	date DATE,
+  	PRIMARY KEY(customerID)
+);
+
+CREATE TABLE discount(
+	itemID INT, 
+	percentOff FLOAT(1, ),
+	PRIMARY KEY(itemID)
+);
+
+INSERT INTO credentials VALUES (12345, "Password");
+INSERT INTO credentials VALUES (54321, "IceCream");
+INSERT INTO credentials VALUES (13542, "PaSsWoRd");
+
+INSERT INTO customer VALUES (12345, 984654, "John", "Doe", "1993-04-21", "497 Lakewood St");
+INSERT INTO customer VALUES (54321, 349165, "Jack", "Lloyd", "2000-05-15", "381 College St");
+INSERT INTO customer VALUES (13542, 765154, "Matt", "Dunn", "1985-01-05", "9101 Main St");
+
+INSERT INTO Cart VALUES (984654, 4567, CURDATE(), 1);
+INSERT INTO Cart VALUES (349165, 2345, CURDATE(), 1);
+INSERT INTO Cart VALUES (765154, 8765, CURDATE(), 1);
+
+INSERT INTO address VALUES ("4927 Perch Dr", 34681, "Charlottesville");
+INSERT INTO address VALUES ("9047 Slip Rd", 97651, "Washington");
+INSERT INTO address VALUES ("529 Gravy Lane", 98462, "San Diego");
+
+INSERT INTO bank account VALUES (94618, 984654, "Bank Of America")
+INSERT INTO bank account VALUES (34981, 349165, "Citi Bank")
+INSERT INTO bank account VALUES (76412, 765154, "Chase Bank")
+
+INSERT INTO suppliers VALUES (4567, "9234 Wards Rd");
+INSERT INTO suppliers VALUES (2345, "1948 Forest Rd");
+INSERT INTO suppliers VALUES (8765, "4028 Link Rd");
+
+INSERT INTO items VALUES (4567, "Pen", 100, 0.75);
+INSERT INTO items VALUES (2345, "Pencil", 100, 0.50);
+INSERT INTO items VALUES (8765, "Notebook", 100, 5.00);
+
+INSERT INTO bill VALUES (984654, 09345, 4567, 34681, CURDATE(), 1.25);
+INSERT INTO bill VALUES (349165, 46831, 2345, 97651, CURDATE(), 5.00);
+INSERT INTO bill VALUES (765154, 07602, 8765, 98462, CURDATE(), 20.00);
+
+INSERT INTO discount VALUES (4567, .20);
+INSERT INTO discount VALUES (2345, .5);
+INSERT INTO discount VALUES (8765, .10);
+
+
+
+
+
+
