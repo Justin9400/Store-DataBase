c61b0e033996db3aaf26f81e6e9b91d374701d9c
Justin9400 <57471411+Justin9400@users.noreply.github.com>
2022-12-02 11:46:21 -0500

    Final Version

--- Databases Final Project.sql
+++ Databases Final Project.sql
@@ -1,16 +1,17 @@
--- DROP DATABASE OnlinePurchaseDB;
+DROP DATABASE OnlinePurchaseDB;
 CREATE DATABASE OnlinePurchaseDB;
 USE OnlinePurchaseDB;
 
+-- Create Tables
 CREATE TABLE credentials(
 	username  VARCHAR(255),
 	password VARCHAR(255),
-  	PRIMARY KEY(userId)
+  	PRIMARY KEY(username)
 );
 
 CREATE TABLE customer(
 	username  VARCHAR(255),
-	customerId INT,
+	customerId INT AUTO_INCREMENT,
     fname VARCHAR(255),
     lname VARCHAR(255),
     DOB DATE,
@@ -71,6 +72,7 @@ CREATE TABLE discount(
 	PRIMARY KEY(itemID)
 );
 
+-- Insert values into tables
 INSERT INTO credentials VALUES ("JohnD123", "Password");
 INSERT INTO credentials VALUES ("JackL456", "IceCream");
 INSERT INTO credentials VALUES ("Matt789", "PaSsWoRd");
@@ -82,7 +84,6 @@ INSERT INTO customer VALUES ("JackL456", 00002, "Jack", "Lloyd", "2000-05-15", "
 INSERT INTO customer VALUES ("Matt789", 00003, "Matt", "Dunn", "1985-01-05", "9101 Main St");
 INSERT INTO customer VALUES ("LizA987", 00004, "Liz", "Alford", "1993-06-29", "1174 Clearly Ln");
 INSERT INTO customer VALUES ("LaurenP654", 00005, "Lauren", "Peach", "1991-12-19", "876 Dreary Ln");
-
 
 INSERT INTO Cart VALUES (00001, 121212, CURDATE(), 1);
 INSERT INTO Cart VALUES (00002, 232323, CURDATE(), 1);
@@ -96,13 +97,11 @@ INSERT INTO address VALUES ("529 Gravy Ln", 49294, "San Diego");
 INSERT INTO address VALUES ("235 Sniper Dr", 59371, "Los Angelos");
 INSERT INTO address VALUES ("854 Lady Slipper Ln", 29743, "Miami");
 
-
 INSERT INTO bankAccount VALUES (01010, 00001, "Bank Of America");
 INSERT INTO bankAccount VALUES (02020, 00002, "Citi Bank");
 INSERT INTO bankAccount VALUES (03030, 00003, "Chase Bank");
 INSERT INTO bankAccount VALUES (04040, 00004, "Credit Union");
 INSERT INTO bankAccount VALUES (05050, 00005, "Bank of the James");
-
 
 INSERT INTO suppliers VALUES (121212, "9234 Wards Rd");
 INSERT INTO suppliers VALUES (232323, "148 Forest Rd");
@@ -116,13 +115,11 @@ INSERT INTO items VALUES (343434, "Notebook", 1000, 2.00);
 INSERT INTO items VALUES (454545, "Backpack", 200, 20.00);
 INSERT INTO items VALUES (565656, "Laptop", 100, 500.00);
 
-
 INSERT INTO bill VALUES (00001, 01010, 09876, 121212, 34681, 1.25, CURDATE());
 INSERT INTO bill VALUES (00002, 02020, 12345, 232323, 97651, 5.00, CURDATE());
 INSERT INTO bill VALUES (00003, 03030, 13579, 343434, 49294, 20.00, CURDATE());
 INSERT INTO bill VALUES (00004, 04040, 24680, 454545, 59371, 20.00, CURDATE());
 INSERT INTO bill VALUES (00005, 05050, 36912, 565656, 29743, 20.00, CURDATE());
-
 
 INSERT INTO discount VALUES (121212, .20);
 INSERT INTO discount VALUES (232323, .50);
@@ -130,27 +127,13 @@ INSERT INTO discount VALUES (343434, .50);
 INSERT INTO discount VALUES (454545, .10);
 INSERT INTO discount VALUES (565656, .05);
 
-CREATE INDEX index_lastname
-ON customer (lName);
-
-SELECT * FROM bill WHERE customerId = '00001' ;
-SELECT * FROM bill WHERE customerId = (SELECT customerId FROM customer c WHERE lname = 'Lloyd');
-SELECT * FROM items;
-SELECT username, password FROM credentials WHERE username ="JohnD123" AND password = 'Password';
-SELECT itemName FROM items WHERE itemId = '121212';
-
 -- index for #6--
 CREATE INDEX last_name on customer(lname);
 
 -- select statement for #7--
-select orderNo as custBill from bill where customerId = '3';
+SELECT * FROM bill WHERE customerId = '00001' ;
+SELECT * FROM bill WHERE customerId = (SELECT customerId FROM customer c WHERE lname = 'Lloyd');
 
 -- select functions for #8--
 create view avgPurchases as select zipcode, sum(total)/count(total) as average from bill group by zipcode order by zipcode asc;
 select * from avgPurchases;
-
--- ERM
--- NORMALIZED WITH COMMENTS
--- EXPLANATION HOW THE PROGRAM WORKS
--- LABOR SHARING TABLE
--- IF WE HAD MORE TIME HOW WOULD WE EXPAND
--- main.py
+++ main.py
@@ -4,45 +4,57 @@ import pandas as pd
 cnx = pymysql.connect(user='root', password='password', host='localhost', database='OnlinePurchaseDB')
 cr = cnx.cursor()
 
+# Continually asks the user if they are a returning user until y or n is given
 while True:
     returningUser = str(input("Are you a returning user? (y/n) "))
 
-    if returningUser == 'y' or returningUser == 'n':
+    if returningUser.lower() == 'y' or returningUser.lower() == 'n':
         break
     else:
         print("Please enter a valid response (y/n)")
 
-
+# If it is a returning user 
 if returningUser.lower() == 'y':
-    while True:
-        userName = str(input("UserId: "))
-        password = str(input("Password: "))
 
+    # Keeps asking the user for a correct username and password 
+    while True:
+        userName = str(input("Username: "))
+        password = str(input("Password: "))
+        # Try except block to catch an error from no result from the select query
         try:
-            query = "SELECT userId, password FROM credentials WHERE userId = {} AND password = {};".format(userName, password)
+            query = f"SELECT username, password FROM credentials WHERE username = '{userName}' AND password = '{password}';"
             result = cr.execute(query)
             if result == 1:
                 break
         except: 
             print("Invalid UserId or Password")
 
+# If the user is not a returning user then create an account
 elif returningUser.lower() == 'n':
     fname = str(input("First Name: "))
     lname = str(input("Last Name: "))
-    DOB = str(input("Date of Birth: "))
+    DOB = str(input("Date of Birth (YYYY-MM-DD): "))
     userName = str(input("Username: "))
     password = str(input("Password: "))
     address = str(input("Home Street Address: "))
+    customerID = 'Null'
+    # Try except block to catch the error of 
     try:
-        query = "INSERT INTO customer VALUES ({}, {}, {}, {}, {}, {})".format(userName, None, fname, lname, DOB, address)
-        result = cr.execute(query)
+        query1 = f"INSERT INTO customer VALUES ('{userName}', {customerID}, '{fname}', '{lname}', '{DOB}', '{address}');"
+        query2 = f"INSERT INTO credentials VALUES ('{userName}', '{password}');"
+
+        cr.execute(query1)
+        cnx.commit()
+        cr.execute(query2)
+        cnx.commit()
     except: 
-        print("Invalid UserId or Password")
+        print()
 
     print("User Successfully Created!")
 
 print("--- Welcome to our store ---")
 
+# Keeps asking the user for one of the options
 while True:
     print("Type 1 if you want to buy some items")
     print("Type 2 if you want to see your 5 most recent purchases")
@@ -53,27 +65,33 @@ while True:
     else:
         print("Please Enter A Valid Choice!")
 
-
 print("--- Customer Interface ---")
+
+# If the user chooses 1 
 if choice == 1:
     query = "SELECT itemId, itemName, price FROM items;"
     nbRes = cr.execute(query)
     itemInfo = {"Item ID" : [], "Item Name" : [], "Price" : []}
     
+    # Keeps printing the first 5 items the user can buy
     while True:
         for itemId, itemName, price in cr.fetchall(): 
             itemInfo["Item ID"].append(itemId)
             itemInfo["Item Name"].append(itemName)
             itemInfo["Price"].append(price)
 
+        # Uses a dataframe to print the info out cleaner
         df = pd.DataFrame.from_dict(itemInfo)
         print(df.to_string(index=False))
 
+        # Keeps asking the user for what item they want to buy until they type -1
         while True:
             purchase = int(input("What would you like to purchase (Input Item ID, -1 to quit): "))
-            
+            print(purchase)
+            # Close program if the user types -1
             if purchase == -1:
                 break
+            # Asks the user how many of the chosen item they would like to buy and prints a message of their choices
             else:
                 try:
                     query = "SELECT itemName FROM items WHERE itemId = {};".format(purchase)
@@ -87,16 +105,20 @@ if choice == 1:
                     print(f"You just purchased {quantity} {result}")
                 except:
                     print("Please enter a valid Item ID or -1 to quit")
+        break
+    print("Program Quitting...")
 
                 
-
+# Prints the 5 most recent bills for the user logged in
 elif choice == 2:
-    query = "SELECT * FROM bill LIMIT 5;"
+    query = "SELECT * FROM bill b, customer c  WHERE b.customerId = c.customerId AND b.customerId = (SELECT customerId FROM customer WHERE username = '{userName}') LIMIT 5;"
+
     nbRes = cr.execute(query)
 
     billInfo = {"Customer ID" : [], "Account Number" : [], "Order Number" : [], "Item ID" : [], "Zipcode" : [], "Total" : [], "Date" : []}
     
-    for customerId, acctNo, orderNo, itemId, zipcode, total, date in cr.fetchall(): 
+    # Prints each of the fields from the bill table for the given user
+    for customerId, acctNo, orderNo, itemId, zipcode, total, date, username, customerId, fname, lname, DOB, HomeAddr in cr.fetchall(): 
         billInfo["Customer ID"].append(customerId)
         billInfo["Account Number"].append(acctNo)
         billInfo["Order Number"].append(orderNo)
@@ -105,5 +127,10 @@ elif choice == 2:
         billInfo["Total"].append(total)
         billInfo["Date"].append(date)
 
+    # Uses a dataframe to print the info out cleaner
     df = pd.DataFrame.from_dict(billInfo)
-    print(df.to_string(index=False))
+
+    if df.empty == True:
+        print("You have no recent bills")
+    else:
+        print(df.to_string(index=False))
